d1d1f2bd1b14bce30473600af342151b5978e4dc
stone535 <dynamikdiamond2.0@gmail.com>
2022-09-27 17:04:35 +0100

    Added an Error text to search result

--- src/Component/CountryList.jsx
+++ src/Component/CountryList.jsx
@@ -2,8 +2,7 @@ import { useGlobalContext } from '../Context';
 import Country from './CountryCard';
 
 const CountryList = () => {
-  const { countries, getCountries, isLoading, query } =
-    useGlobalContext();
+  const { query } = useGlobalContext();
 
   const result = query.map((country, index) => {
     return (
@@ -15,9 +14,19 @@ const CountryList = () => {
     );
   });
 
+  const content = result?.length ? (
+    result
+  ) : (
+    <article>
+      <h1 className="text-center font-bold text-[1rem]">
+        No Matching Country
+      </h1>
+    </article>
+  );
+
   return (
     <div className=" grid  place-items-center gap-[3rem] py-[3rem] sm:grid-cols-2 lg:grid-cols-4 ">
-      {result}
+      {content}
     </div>
   );
 };
